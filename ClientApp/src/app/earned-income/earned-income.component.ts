623f662b5ae0a6b66fb3324f17fc785eee1f414b
Alex Rogers <alex.rogers823@gmail.com>
2021-04-10 21:44:23 -0400

    added files for earned income component

---
+++ ClientApp/src/app/earned-income/earned-income.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-earned-income',
+  templateUrl: './earned-income.component.html',
+  styleUrls: ['./earned-income.component.css']
+})
+export class EarnedIncomeComponent {}
