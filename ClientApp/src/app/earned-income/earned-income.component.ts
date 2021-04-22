6453ed57f5780efbd1bd78e00636eb3b72c68d27
Alex Rogers <alex.rogers823@gmail.com>
2021-04-21 22:56:32 -0400

    refactored earned income to use content projection from emerald component

--- ClientApp/src/app/earned-income/earned-income.component.ts
+++ ClientApp/src/app/earned-income/earned-income.component.ts
@@ -8,8 +8,7 @@ import { Component } from "@angular/core";
 export class EarnedIncomeComponent {
   isEditing:boolean = false
   goalIncome = 5000
-  hasEmerald:boolean = this.currentIncome > this.goalIncome
-
+  
   earned = {
     jobs: [
       {
@@ -24,12 +23,14 @@ export class EarnedIncomeComponent {
     currentIncome: 6000,
     articles: ['Article 1', 'Article 2', 'Article 3']
   }
-
+  
   quote = {
     quote: '"When you work on something that only has the capacity to make you 5 dollars, it does not matter how much harder you work – the most you will make is 5 dollars."',
     author: 'Idowu Koyenikan'
   }
-
+  
+  hasEmerald:boolean = this.earned.currentIncome > this.goalIncome
+  
   editSalary() {
     this.isEditing = true
   }
