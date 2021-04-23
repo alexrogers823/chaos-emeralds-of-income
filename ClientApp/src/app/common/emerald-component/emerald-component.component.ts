1abc3b59b3c0e2efac316c857fcf833e6cd973f3
Alex Rogers <alex.rogers823@gmail.com>
2021-04-22 20:36:46 -0400

    used property binding to communicate between earned income component and emerald component

--- ClientApp/src/app/common/emerald-component/emerald-component.component.ts
+++ ClientApp/src/app/common/emerald-component/emerald-component.component.ts
@@ -1,9 +1,11 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
+import { IEmerald } from "src/app/shared/emerald.model";
 
 @Component({
   selector: 'emerald-component',
   templateUrl: './emerald-component.component.html'
 })
 export class EmeraldComponent {
-  
+  @Input() emerald:IEmerald;
+  @Input() hasEmerald:boolean;
 }
--- ClientApp/src/app/earned-income/earned-income.component.ts
+++ ClientApp/src/app/earned-income/earned-income.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { IEmerald } from "../shared/emerald.model";
 
 @Component({
   selector: 'app-earned-income',
@@ -6,9 +7,21 @@ import { Component } from "@angular/core";
   styleUrls: ['./earned-income.component.css']
 })
 export class EarnedIncomeComponent {
-  isEditing:boolean = false
-  goalIncome = 5000
-  
+  isEditing:boolean = false;
+  private goalIncome:number = 5000;
+  earnedIncomeEmerald:IEmerald = {
+    title: 'Earned Income',
+    quote: '"When you work on something that only has the capacity to make you 5 dollars, it does not matter how much harder you work – the most you will make is 5 dollars."',
+    author: 'Idowu Koyenikan',
+    goal: this.goalIncome.toString(),
+    emeraldImageUrl: 'placeholder-emerald.jpg',
+    resources: [
+      'resource 1',
+      'resource 2',
+      'resource 3'
+    ]
+  };
+
   earned = {
     jobs: [
       {
@@ -21,17 +34,13 @@ export class EarnedIncomeComponent {
       }
     ],
     currentIncome: 6000,
-    articles: ['Article 1', 'Article 2', 'Article 3']
   }
-  
-  quote = {
-    quote: '"When you work on something that only has the capacity to make you 5 dollars, it does not matter how much harder you work – the most you will make is 5 dollars."',
-    author: 'Idowu Koyenikan'
-  }
-  
-  hasEmerald:boolean = this.earned.currentIncome > this.goalIncome
   
   editSalary() {
     this.isEditing = true
+  }
+
+  hasEarnedIncomeEmerald() {
+    return this.earned.currentIncome > this.goalIncome;
   }
 }
