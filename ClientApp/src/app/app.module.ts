ddbf5f2ada23f830cd82d9ed385163c3097f0819
Alex Rogers <alex.rogers823@gmail.com>
2021-04-26 19:48:18 -0400

    added earned income component to app module and routing

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { EarnedIncomeComponent } from './components/earned-income/earned-income.component';
+import { EmeraldComponent } from './components/common/emerald-component/emerald-component.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    EmeraldComponent,
+    EarnedIncomeComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,6 +30,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'earned-income', component: EarnedIncomeComponent}
     ])
   ],
   providers: [],
