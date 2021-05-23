52b99f20ca6f3b60ba35d557f14b982743612a7b
Alex Rogers <alex.rogers823@gmail.com>
2021-05-23 17:21:09 -0400

    added all emerald images to main page

--- ClientApp/src/app/components/main-page/main-page.component.ts
+++ ClientApp/src/app/components/main-page/main-page.component.ts
@@ -5,5 +5,13 @@ import { Component } from "@angular/core";
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent {
-  
+  allEmeralds:any[] = [
+    { source: 'Earned Income', imageLink: 'assets/green-emerald.png' },
+    { source: 'Interest Income', imageLink: 'assets/purple-emerald.png' },
+    { source: 'Capital Gains', imageLink: 'assets/yellow-emerald.png' },
+    { source: 'Dividend Income', imageLink: 'assets/blue-emerald.png' },
+    { source: 'Rental Income', imageLink: 'assets/lightblue-emerald.png' },
+    { source: 'Business Income', imageLink: 'assets/red-emerald.png' },
+    { source: 'Royalty Income', imageLink: 'assets/white-emerald.png' }
+  ]
 }
