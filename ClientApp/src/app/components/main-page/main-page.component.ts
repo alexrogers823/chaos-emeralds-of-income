3688c3d989d61ed99d49af21b89ce2b47fd37f2c
Alex Rogers <alex.rogers823@gmail.com>
2021-05-25 22:00:49 -0400

    added page link to navigate from main page

--- ClientApp/src/app/components/main-page/main-page.component.ts
+++ ClientApp/src/app/components/main-page/main-page.component.ts
@@ -7,12 +7,12 @@ import { Component } from "@angular/core";
 })
 export class MainPageComponent {
   allEmeralds:any[] = [
-    { source: 'Earned Income', imageLink: 'assets/green-emerald.png' },
-    { source: 'Interest Income', imageLink: 'assets/purple-emerald.png' },
-    { source: 'Capital Gains', imageLink: 'assets/yellow-emerald.png' },
-    { source: 'Dividend Income', imageLink: 'assets/blue-emerald.png' },
-    { source: 'Rental Income', imageLink: 'assets/lightblue-emerald.png' },
-    { source: 'Business Income', imageLink: 'assets/red-emerald.png' },
-    { source: 'Royalty Income', imageLink: 'assets/white-emerald.png' }
+    { source: 'Earned Income', pageLink: '/earned-income', imageLink: 'assets/green-emerald.png' },
+    { source: 'Interest Income', pageLink: '/interest-income', imageLink: 'assets/purple-emerald.png' },
+    { source: 'Capital Gains', pageLink: '/capital-gains', imageLink: 'assets/yellow-emerald.png' },
+    { source: 'Dividend Income', pageLink: '/dividend-income', imageLink: 'assets/blue-emerald.png' },
+    { source: 'Rental Income', pageLink: '/rental-income', imageLink: 'assets/lightblue-emerald.png' },
+    { source: 'Business Income', pageLink: '/business-income', imageLink: 'assets/red-emerald.png' },
+    { source: 'Royalty Income', pageLink: '/royalty-income', imageLink: 'assets/white-emerald.png' }
   ]
 }
