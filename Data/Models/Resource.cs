1033dda123da34d21c5179072e8a45569cbacf0c
Alex Rogers <alex.rogers823@gmail.com>
2021-06-11 23:40:32 -0400

    added resource model to backend for article information

---
+++ Data/Models/Resource.cs
@@ -0,0 +1,9 @@
+namespace ChaosEmeraldsOfIncome.Controllers
+{
+    public class Resource
+    {
+        public string Name { get; set; }
+        public string Link { get; set; }
+        public int IncomeId { get; set; }
+    }
+}
