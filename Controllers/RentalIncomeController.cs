19b465e962e4f71d3b9d3c0d910900ab35527078
Alex Rogers <alex.rogers823@gmail.com>
2021-07-05 16:29:51 -0400

    set up rental income controller and get rental income endpoint

---
+++ Controllers/RentalIncomeController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChaosEmeraldsOfIncome.Controllers
+{
+    [ApiController]
+    [Route("api/RentalIncome")]
+    public class RentalIncomeController : ControllerBase
+    {
+        private readonly IRentalIncomeRepo _repo;
+
+        public RentalIncomeController(IRentalIncomeRepo repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public ActionResult<RentalIncome> GetRentalIncome()
+        {
+            var rentalIncome = _repo.GetAllRentalProperties();
+            return Ok(rentalIncome);
+        }
+    }
+}
--- Data/MockRentalIncomeRepo.cs
+++ Data/MockRentalIncomeRepo.cs
@@ -16,7 +16,14 @@ namespace ChaosEmeraldsOfIncome.Controllers
 
         public IEnumerable<RentalIncome> GetAllRentalProperties()
         {
-            throw new System.NotImplementedException();
+            var properties = new List<RentalIncome> 
+            {
+                new RentalIncome{ Id=1, Property="Lofts at Summerhill", Mortgage=2850.00, Rent=3250, Frequency="Monthly"},
+                new RentalIncome{ Id=2, Property="Springwood Flats", Mortgage=1675.25, Rent=2200, Frequency="Monthly"},
+                new RentalIncome{ Id=3, Property="Wintergreen Apartments", Mortgage=875.00, Rent=1000, Frequency="Monthly"}
+            };
+
+            return properties;
         }
     }
 }
