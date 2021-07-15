baa9e5125418046f7c6a5f7253b6b9b53bf1a4a3
Alex Rogers <alex.rogers823@gmail.com>
2021-07-15 08:38:26 -0500

    completed information for dividend income component

--- ClientApp/src/app/components/dividend-income/dividend-income.component.ts
+++ ClientApp/src/app/components/dividend-income/dividend-income.component.ts
@@ -1,17 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { from } from "rxjs";
+import { mergeScan, scan, tap } from "rxjs/operators";
+import { DividendIncomeService } from "src/app/services/dividend-income.service";
 import { Emerald } from "src/app/shared/emerald.model";
+import { DividendIncome } from "./interfaces/dividend-income.model";
 
 @Component({
   selector: 'dividend-income',
   templateUrl: './dividend-income.component.html'
 })
-export class DividendIncomeComponent {
-  private goalDividendIncome = 1000;
+export class DividendIncomeComponent implements OnInit {
+  private currentDividendIncome:number;
+  private goalDividendIncome:number = 1000;
   dividendIncomeEmerald:Emerald = {
     id: 6,
     title: 'Dividend Income',
-    quote: 'test quote',
-    author: 'test author',
+    quote: 'I do not own a single security anywhere that doesn\'t pay a dividend, and I formed a mutual-fund company with that very simple philosophy',
+    author: 'Kevin O\'Leary',
     goal: `$${this.goalDividendIncome}/mo from dividends`,
     emeraldImageUrl: 'assets/blue-emerald.png',
     resources: [
@@ -21,7 +26,30 @@ export class DividendIncomeComponent {
     ]
   }
 
+  constructor(private dividendIncomeService:DividendIncomeService) {}
+
+  dividendIncome$ = this.dividendIncomeService.dividendIncome$
+
+  dividendIncomeTotal$ = this.dividendIncomeService.dividendIncome$
+    .pipe(
+      mergeScan((acc:number, curr:DividendIncome[]) => {
+        return from(curr)
+          .pipe(
+            scan((start:number, dividend:DividendIncome) => start + dividend.dividendPayout, 0)
+          )
+      }, 0),
+      tap(output => console.log('total', output))
+    );
+
+  ngOnInit() {
+    this.dividendIncomeTotal$.subscribe(income => {
+      this.currentDividendIncome = income;
+    })
+  }
+
+  
+
   hasDividendIncomeEmerald() {
-    return 500 > this.goalDividendIncome;
+    return this.currentDividendIncome > this.goalDividendIncome;
   }
 }
