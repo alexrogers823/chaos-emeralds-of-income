1128d01434ba8fff990d84e84859468c273b835b
Alex Rogers <alex.rogers823@gmail.com>
2021-07-18 21:24:50 -0400

    added detail to get business income endpoint and controller

---
+++ Controllers/BusinessIncomeController.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChaosEmeraldsOfIncome.Controllers
+{
+    [ApiController]
+    [Route("api/BusinessIncome")]
+    public class BusinessIncomeController : ControllerBase
+    {
+        private readonly IBusinessIncomeRepo _repo;
+
+        public BusinessIncomeController(IBusinessIncomeRepo repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<BusinessIncome>> GetAllBusinessIncome()
+        {
+            var businesses = _repo.GetAllBusinessIncome();
+            return Ok(businesses);
+        }
+    }
+}
--- Data/MockBusinessIncomeRepo.cs
+++ Data/MockBusinessIncomeRepo.cs
@@ -11,7 +11,13 @@ namespace ChaosEmeraldsOfIncome.Controllers
 
         public IEnumerable<BusinessIncome> GetAllBusinessIncome()
         {
-            throw new System.NotImplementedException();
+            var businesses = new List<BusinessIncome>
+            {
+                new BusinessIncome{Id=21, BusinessName="Rogers Incorporated", Revenue=10800, Expenses=4128, Profit=6672, IsCurrent=true},
+                new BusinessIncome{Id=22, BusinessName="Virgocity", Revenue=18966, Expenses=11585, Profit=7381, IsCurrent=true},
+                new BusinessIncome{Id=23, BusinessName="Sinners and Saints", Revenue=22540, Expenses=12400, Profit=10140, IsCurrent=true},
+            };
+            return businesses;
         }
 
         public void UpdateBusiness()
