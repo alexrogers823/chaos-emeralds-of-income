db9b50159befd42d105bbe4731a70ab89700e374
Alex Rogers <alex.rogers823@gmail.com>
2021-07-25 14:02:43 -0400

    created db fields for all sources of income and users

---
+++ Data/CEOIContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ChaosEmeraldsOfIncome.Controllers
+{
+    public class CEOIContext : DbContext
+    {
+        public CEOIContext(DbContextOptions<CEOIContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<EarnedIncome> EarnedIncome { get; set; }
+        public DbSet<InterestIncome> InterestIncome { get; set; }
+        public DbSet<RentalIncome> RentalIncome { get; set; }
+        public DbSet<CapitalGains> CapitalGains { get; set; }
+        public DbSet<DividendIncome> DividendIncome { get; set; }
+        public DbSet<RoyaltyIncome> RoyaltyIncome { get; set; }
+        public DbSet<BusinessIncome> BusinessIncome { get; set; }
+        public DbSet<User> User { get; set; }
+    }
+}
