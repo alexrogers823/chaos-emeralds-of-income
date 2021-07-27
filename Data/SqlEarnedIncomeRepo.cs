d9d39894d13c2dc8a8542258529f0a07ce51662e
Alex Rogers <alex.rogers823@gmail.com>
2021-07-26 21:43:20 -0400

    created sql file for earned income to replace mock

---
+++ Data/SqlEarnedIncomeRepo.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChaosEmeraldsOfIncome.Controllers
+{
+    public class SqlEarnedIncomeRepo : IEarnedIncomeRepo
+    {
+        private readonly CEOIContext _context;
+
+        public SqlEarnedIncomeRepo(CEOIContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<EarnedIncome> GetEarnedIncome()
+        {
+            return _context.EarnedIncome.ToList();
+        }
+
+        // change this to void 
+        public IEnumerable<EarnedIncome> InsertEarnedIncome(EarnedIncome income)
+        {
+            if (income == null)
+            {
+                throw new ArgumentNullException(nameof(income));
+            }
+
+            _context.EarnedIncome.Add(income);
+
+        }
+    }
+}
