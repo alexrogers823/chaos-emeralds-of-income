89dc05d1feee80623e4c7c095d38271ce9467329
Alex Rogers <alex.rogers823@gmail.com>
2021-08-22 10:39:42 -0400

    added data attributes to earned and interest income

--- Data/Models/EarnedIncome.cs
+++ Data/Models/EarnedIncome.cs
@@ -12,7 +12,9 @@ namespace ChaosEmeraldsOfIncome.Models
         [ForeignKey("UserId")]
         public User User { get; set; }
         [Required]
+        [StringLength(50, ErrorMessage = "{0} cannot exceed {1} characters")]
         public string IncomeTitle { get; set; }
+        [Range(5, 999999, ErrorMessage = "Income should be between {0} and {1}")]
         public int IncomeAmount { get; set; }
         [Required]
         public string Frequency { get; set; }
--- Data/Models/InterestIncome.cs
+++ Data/Models/InterestIncome.cs
@@ -12,8 +12,10 @@ namespace ChaosEmeraldsOfIncome.Models
         [ForeignKey("UserId")]
         public User User { get; set; }
         [Required]
+        [StringLength(50, ErrorMessage = "{0} cannot exceed {1} characters")]
         public string Name { get; set; }
         public double? TotalSavings { get; set; }
+        [Range(0.01, 25, ErrorMessage = "Percentage must be between {0} and {1}")]
         public double InterestPercent { get; set; }
         public double InterestDollar { get; set; }
         // field used for net worth calculation 
