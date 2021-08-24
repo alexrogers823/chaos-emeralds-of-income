8275b1b63f4c9933b53db358889d98e2f2785e4f
Alex Rogers <alex.rogers823@gmail.com>
2021-08-24 18:56:03 -0400

    created read dto for interest income

---
+++ Dtos/InterestIncome/InterestIncomeReadDto.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ChaosEmeraldsOfIncome.Dtos
+{
+    public class InterestIncomeReadDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public double? TotalSavings { get; set; }
+        public double InterestPercent { get; set; }
+        public double InterestDollar { get; set; }
+        // field used for net worth calculation 
+        public bool IsActive { get; set; }
+        public DateTime? DateAdded { get; set; }
+        public DateTime? LastUpdated { get; set; }
+    }
+}
